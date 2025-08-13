ca9cf23e026150ce867885c1916be58f3b6627e9
kaca100janovic <kaca.katarina.stojanovic@gmail.com>
2025-08-13 23:56:02 +0200

    16 test

--- tests/problem_user.spec.ts
+++ tests/problem_user.spec.ts
@@ -1,49 +1,25 @@
 import { test, expect } from '@playwright/test';
 import {loginProblemUser ,userInformation, yourCart ,emptyFirstName, emptyLastName,emptyPostalCode, addItemsToCart, items, sortedAZ, sortedZA, checkEmptyCheckoutFields} from '../utils/helpers';
 
+// test('error input', async ({ page }) => {
+//   await loginProblemUser(page);
+//   await addItemsToCart(page, [items[0]]);
 
 
-
-test('No error message on empty fields but show alert on continue', async ({ page }) => {
-  await loginProblemUser(page);
- await addItemsToCart(page, [items[0]]);
-
-  // Idi na korpu i checkout
-  await page.locator('.shopping_cart_link').click();
-  await page.locator('#checkout').click();
-
-  // Ostavi prazna polja
-  await page.fill('#first-name', '');
-  await page.fill('#last-name', '');
-  await page.fill('#postal-code', '');
-
-  // Klik na Continue
-  await page.locator('#continue').click();
-
-  // Provera da je error poruka za First Name vidljiva
-  await expect(page.locator('.error-message-container')).toHaveText('Error: First Name is required');
-});
-
-test('error input', async ({ page }) => {
-  await loginProblemUser(page);
- await addItemsToCart(page, [items[0]]);
-
-  // Idi na korpu i checkout
-  await page.locator('.shopping_cart_link').click();
-  await page.locator('#checkout').click();
-
-  // Ostavi prazna polja
-  await page.fill('#first-name', 'Katarina');
-  await page.fill('#last-name', 'Stojanovic');
-  await page.fill('#postal-code', '1236');
+//   await page.locator('.shopping_cart_link').click();
+//   await page.locator('#checkout').click();
 
 
-  await page.locator('#continue').click();
+//   await page.fill('#first-name', 'Katarina');
+//   await page.fill('#last-name', 'Stojanovic');
+//   await page.fill('#postal-code', '1236');
 
 
-  await expect(page.locator('#first-name')).toHaveText('Katarina');
+//   await expect(page.locator('#first-name')).toHaveValue('Katarina');
+//   await expect(page.locator('#last-name')).toHaveValue('Stojanovic');
+//   await expect(page.locator('#postal-code')).toHaveValue('1236');
+ 
 
 
+// });
 
-
-});
--- tests/standard_user.spec.ts
+++ tests/standard_user.spec.ts
@@ -3,22 +3,44 @@ import {initializeLoginPage, login,userInformation, yourCart ,emptyFirstName, em
 
 
 test('Successful login', async ({ page }) => {
-  const { userName, password, signIn } = await initializeLoginPage(page);
+const { userName, password, signIn } = await initializeLoginPage(page);
 
-  await userName.fill('standard_user');
-  await password.fill('secret_sauce');
-  await signIn.click();
+await userName.fill('standard_user');
+await password.fill('secret_sauce');
+await signIn.click();
 
-  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
 
-test('Buy one item', async({page}) =>{
-  await login(page);
+test('Incorect password', async ({ page }) => {
+const { userName, password, signIn } = await initializeLoginPage(page);
 
- const item = page.locator('.inventory_item_name', { hasText: 'Sauce Labs Backpack' });
-  await item.click();
-  const addToCartBtn = page.locator('#add-to-cart');
-  await addToCartBtn.click();
+await userName.fill('standard_user');
+await password.fill('standard_user');
+await signIn.click();
+
+const errorMessage = page.locator('.error-message-container');
+await expect(errorMessage).toBeVisible();
+await expect(errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service')
+
+});
+
+test('Logout', async ({ page }) => {
+await login(page);
+await page.locator('#react-burger-menu-btn').click();
+await page.locator('#logout_sidebar_link').click();
+
+await expect(page).toHaveURL('https://www.saucedemo.com/');
+});
+
+
+test('Buy one item', async({page}) =>{
+await login(page);
+
+const item = page.locator('.inventory_item_name', { hasText: 'Sauce Labs Backpack' });
+await item.click();
+const addToCartBtn = page.locator('#add-to-cart');
+await addToCartBtn.click();
 const cart = page.locator('.shopping_cart_badge');
 await cart.click();
 const checkout = page.locator('#checkout');
@@ -34,7 +56,7 @@ await expect(page.locator('.app_logo')).toHaveText('Swag Labs');
 });
 
 test('Remove item from cart', async({page}) =>{
-  await login(page);
+await login(page);
 
 const item = page.locator('#add-to-cart-sauce-labs-backpack');
 await item.click();
@@ -47,7 +69,7 @@ await expect(page.locator('.cart_item')).toHaveCount(0);
 
 
 test('empty first name', async({page}) =>{
-  await login(page);
+await login(page);
 await yourCart(page);
 await emptyFirstName(page);
 
@@ -56,7 +78,7 @@ await expect(page.locator('.error-message-container')).toHaveText('Error: First 
 });
 
 test('empty last name', async({page}) =>{
-  await login(page);
+await login(page);
 await yourCart(page);
 await emptyLastName(page);
 
@@ -65,7 +87,7 @@ await expect(page.locator('.error-message-container')).toHaveText('Error: Last N
 });
 
 test('empty postal code', async({page}) =>{
-  await login(page);
+await login(page);
 await yourCart(page);
 await emptyPostalCode(page);
 
@@ -74,14 +96,14 @@ await expect(page.locator('.error-message-container')).toHaveText('Error: Postal
 });
 
 test('dodavanje vise itema', async({page}) =>{
-  await login(page);
-  const selectedItems = [items[0],items[1], items[2]];
+await login(page);
+const selectedItems = [items[0],items[1], items[2]];
 
-  await addItemsToCart(page, selectedItems);
+await addItemsToCart(page, selectedItems);
 
-  const checkout = page.locator('#checkout');
-  await checkout.click();
- 
+const checkout = page.locator('#checkout');
+await checkout.click();
+
 await userInformation(page);
 await page.locator('#continue').click();
 
@@ -97,28 +119,95 @@ await expect(page.locator('.app_logo')).toHaveText('Swag Labs');
 
 
 test('Product sort AZ', async({page}) =>{
-  await login(page);
+await login(page);
 
-  const productSort = page.locator('.product_sort_container');
-  // await productSort.click();
+const productSort = page.locator('.product_sort_container');
+// await productSort.click();
 
-  await productSort.selectOption('az');
+await productSort.selectOption('az');
 
-  await expect(productSort).toHaveValue('az');
+await expect(productSort).toHaveValue('az');
 
-  await sortedAZ(page);
+await sortedAZ(page);
 
 });
 
 test('Product sort ZA', async({page}) =>{
-  await login(page);
+await login(page);
 
-  const productSort = page.locator('.product_sort_container');
+const productSort = page.locator('.product_sort_container');
 
-  await productSort.selectOption('za');
+await productSort.selectOption('za');
 
-  await expect(productSort).toHaveValue('za');
+await expect(productSort).toHaveValue('za');
 
-  await sortedZA(page);
+await sortedZA(page);
 
 });
+
+test('Tab button', async({page}) =>{
+await login(page);
+await yourCart(page);
+await expect(page).toHaveURL('https://www.saucedemo.com/checkout-step-one.html');
+await page.click('#first-name');
+await page.keyboard.type('Katarina');
+
+await page.keyboard.press('Tab');
+await expect(page.locator('#last-name')).toBeFocused();
+
+await page.keyboard.press('Tab');
+await expect(page.locator('#postal-code')).toBeFocused();
+});
+
+test('Cart number', async({page}) =>{
+await login(page);
+const selectedItems = [items[0],items[1], items[2]];
+
+await addItemsToCart(page, selectedItems);
+
+  await expect(page.locator('#shopping_cart_container')).toHaveText('3');
+
+});
+
+test('error input', async ({ page }) => {
+  await login(page);
+  await addItemsToCart(page, [items[0]]);
+
+
+  await page.locator('.shopping_cart_link').click();
+  await page.locator('#checkout').click();
+
+
+  await page.fill('#first-name', 'Katarina');
+  await page.fill('#last-name', 'Stojanovic');
+  await page.fill('#postal-code', '1236');
+
+
+  await expect(page.locator('#first-name')).toHaveValue('Katarina');
+  await expect(page.locator('#last-name')).toHaveValue('Stojanovic');
+  await expect(page.locator('#postal-code')).toHaveValue('1236');
+ 
+
+
+});
+
+test('No error message on empty fields but show alert on continue', async ({ page }) => {
+  await login(page);
+ await addItemsToCart(page, [items[0]]);
+
+  // Idi na korpu i checkout
+  await page.locator('.shopping_cart_link').click();
+  await page.locator('#checkout').click();
+
+  // Ostavi prazna polja
+  await page.fill('#first-name', '');
+  await page.fill('#last-name', '');
+  await page.fill('#postal-code', '');
+
+  // Klik na Continue
+  await page.locator('#continue').click();
+
+  // Provera da je error poruka za First Name vidljiva
+  await expect(page.locator('.error-message-container')).toHaveText('Error: First Name is required');
+});
+
